a540725a71bde07b3479359f71e5ef0f90b06917
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-04-26 16:21:49 +0900

    Remove unnecessary code and files

--- src/dom.rs
+++
@@ -1,8 +0,0 @@
-use html_parser::Dom;
-use reqwest::get;
-#[tokio::main]
-pub async fn dom(url: &str) -> Result<Dom, Box<dyn std::error::Error>> {
-    let html = get(url).await?.text().await?;
-    let dom = Dom::parse(&html)?;
-    Ok(dom)
-}
